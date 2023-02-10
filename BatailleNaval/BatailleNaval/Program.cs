31e95ac628ce717c198fe803c5523fad257242dc
Grootaers Romane <210469@CCM-Asty-Moulin.local>
2023-02-10 16:20:17 +0100

    rien

--- BatailleNaval/BatailleNaval/Program.cs
+++ BatailleNaval/BatailleNaval/Program.cs
@@ -6,23 +6,23 @@ namespace BatailleNaval
     {
         static void Main(string[] args)
         {
-            int[,] grille2D = new int[10, 10];
-            char[] alpha = "ABCDEFGHIJ".ToCharArray();
-            Console.WriteLine(alpha);
+            string[,] grille2D = new string[10, 10];
+            string[] alpha = "ABCDEFGHIJ".ToCharArray();
             AfficherGrille(grille2D, alpha);
             Console.WriteLine(grille2D);
         }
-        static void AfficherGrille(int[,] grille2D, char[] alpha)
+        static void AfficherGrille(string[,] grille2D, string[] alpha)
         {
+            int n = 0;
             int nbr = 1;
             for (int compteurColonne = 0; compteurColonne < 10; compteurColonne++)
             {
-                grille2D[0, compteurColonne] = nbr;
+                grille2D[0, compteurColonne] = Convert.ToString(nbr);
                 nbr++;
             }
             for (int compteurLigne = 0; compteurLigne < 10; compteurLigne++)
             {
-                grille2D[compteurLigne, 0] = nbr;
+                grille2D[compteurLigne, 0] = alpha[n] ;
                 nbr++;
                 Console.WriteLine();
             }
