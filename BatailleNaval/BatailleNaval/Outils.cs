b6e6ad16f2e6817832148ad75e1a4115a7bbe03e
RomaneG6 <210469@site.asty-moulin.be>
2023-03-09 21:25:08 +0100

    derniere version

--- BatailleNaval/BatailleNaval/Outils.cs
+++ BatailleNaval/BatailleNaval/Outils.cs
@@ -6,28 +6,50 @@ namespace BatailleNaval
 {
     internal class Outils
     {
-        public void AfficherGrille(string[,] grille2D, out string[] alpha, out string resultat)
+        public void AfficherGrille(string[,] tab2D, out string[] alpha, out string resultat)
         {
             alpha = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
             resultat = "";
             int nbr = 0;
-            for (int l = 0; l <= grille2D.GetLength(0) - 1; l++)
+            for (int l = 0; l <= tab2D.GetLength(0) - 1; l++)
             {
-                for (int c = 0; c <= grille2D.GetLength(1) - 1; c++)
+                for (int c = 0; c <= tab2D.GetLength(1) - 1; c++)
                 {
-                    grille2D[c, l] = "0";
-                    grille2D[c, 0] = nbr.ToString();
+                    tab2D[c, l] = "0";
+                    tab2D[c, 0] = nbr.ToString();
                     nbr++;
-                    for (int n = 0; n < alpha.Length; n++)
-                    {
-                        grille2D[0, l] = alpha[n];
-                        grille2D[0, 0] = "0";
-                    }
-
-                    resultat = resultat + grille2D[c, l] + " ";
+                    //for (int n = 0; n < 10; n++)
+                    //{
+                        //tab2D[0, l] = alpha[n];
+                        tab2D[0, 1] = alpha[0];
+                        tab2D[0, 2] = alpha[1];
+                        tab2D[0, 3] = alpha[2];
+                        tab2D[0, 4] = alpha[3];
+                        tab2D[0, 5] = alpha[4];
+                        tab2D[0, 6] = alpha[5];
+                        tab2D[0, 7] = alpha[6];
+                        tab2D[0, 8] = alpha[7];
+                        tab2D[0, 9] = alpha[8];
+                        tab2D[0, 10] = alpha[9];
+                    //}
+                    resultat = resultat + tab2D[c, l] + " " + " " + " " + " " + " ";
                 }
-                resultat = resultat + "\n";
+                resultat = resultat + "\n" + "\n";
             }
+        }
+        public void AfficherGrilleETBateau(int premBordColonne, string premBordLigne, int dernBordColonne, string dernBordLigne, string[] bateau, string[,] BJgrille2D, string resultat)
+        {
+            for (int l = 0; l < BJgrille2D.GetLength(0); l++)
+            {
+                for (int c = 0; c < BJgrille2D.GetLength(1); c++)
+                {   
+                    if (premBordColonne == c)
+                    {
+                        premBordColonne = 1;
+                    }
+                }
+            }
+            resultat = resultat + premBordColonne;
         }
     }
 }
--- BatailleNaval/BatailleNaval/Program.cs
+++ BatailleNaval/BatailleNaval/Program.cs
@@ -8,26 +8,126 @@ namespace BatailleNaval
         {
             Outils tools = new Outils();
             string resultat;
-            string[,] grille2D = new string[11, 11];
+            string[,] BJgrille2D = new string[11, 11];
+            string[,] Jgrille2D = new string[11, 11];
             string[] alpha;
-            tools.AfficherGrille(grille2D,out alpha, out resultat);
-            Console.WriteLine(resultat);
-            Console.WriteLine(resultat);
-       
-            //int n = 0;
-            //int nbr = 1;
-            //for (int compteurColonne = 0; compteurColonne < grille2D.GetLength(0) - 1; compteurColonne++)
-            //{
+            string[] porteA = new string[5];
+            string[] croisseur = new string[4];
+            string[] contreC = new string[3];
+            string[] sousM = new string[3];
+            string[] torpilleur = new string[2];
+            string reponse = "";
+            string[] bateau = new string[] { "porte avion (5 cases)", "croiseur (4 cases)", "contre-croiseur (3 cases)", "sous-marin (3 cases)", "torpilleur (2 cases)" };
+            int premBordColonne;
+            string premBordLigne;
+            int dernBordColonne;
+            string dernBordLigne;
 
-            //    grille2D[0, compteurColonne] = Convert.ToChar(nbr);
-            //    nbr++;
-            //}
-            //for (int compteurLigne = 0; compteurLigne < grille2D.GetLength(1) - 1; compteurLigne++)
-            //{
-            //    grille2D[compteurLigne, 0] = alpha[n] ;
-            //    nbr++;
-            //    Console.WriteLine();
-            //}
+            Console.WriteLine("Souhaitez-vous jouer à deux ou tout seul ? Tapez deux ou seul");
+            reponse = Console.ReadLine();
+            Console.WriteLine("\n");
+            if (reponse == "seul")
+            {
+                //tools.AfficherGrille(Jgrille2D,out alpha, out resultat);grille du joueur où il essaiera de deviner les bateau de l'adversaire
+                //Console.WriteLine(resultat + "\n" + "\n");
+                tools.AfficherGrille(BJgrille2D, out alpha, out resultat);// grille du joueur où il placera ses bateaux
+                Console.WriteLine(resultat);
+                Console.WriteLine("Placer les différents bateau à votre disposition :\n\n");
+                Console.WriteLine("Ecrivez d'abord les extémités de la où vous voulez mettre vos bateau\nEn commmencant par les premières extrémités puis les dernières\n");
+
+                for (int compteur = 0; compteur < bateau.Length; compteur++)
+                {
+                    Console.WriteLine("Placez le " + bateau[compteur] + "\n");
+
+                    Console.WriteLine("Premières coordonnées :\n");
+                    Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                    premBordColonne = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                    premBordLigne = Console.ReadLine();
+
+                    Console.WriteLine("Dernières coordonnées :\n");
+                    Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                    dernBordColonne = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                    dernBordLigne = Console.ReadLine();
+                    tools.AfficherGrilleETBateau(premBordColonne, premBordLigne, dernBordColonne, dernBordLigne, bateau, BJgrille2D, resultat);
+                }
+
+                //Console.WriteLine("Placez le porte avion (5 cases)\n");
+
+                //Console.WriteLine("Premières coordonnées :\n");
+                //Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                //premBordColonne = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                //premBordLigne = int.Parse(Console.ReadLine());
+
+                //Console.WriteLine("Dernières coordonnées :\n");
+                //Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                //dernBordColonne = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                //dernBordLigne = int.Parse(Console.ReadLine());
+                //tools.AfficherGrilleETBateau();
+
+                //Console.WriteLine("Placez le croiseur (4 cases)\n");
+
+                //Console.WriteLine("Premières coordonnées :\n");
+                //Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                //premBordColonne = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                //premBordLigne = int.Parse(Console.ReadLine());
+
+                //Console.WriteLine("Dernières coordonnées :\n");
+                //Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                //dernBordColonne = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                //dernBordLigne = int.Parse(Console.ReadLine());
+                //tools.AfficherGrilleETBateau();
+
+                //Console.WriteLine("Placez le contre-croiseur (3 cases)\n");
+
+                //Console.WriteLine("Premières coordonnées :\n");
+                //Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                //premBordColonne = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                //premBordLigne = int.Parse(Console.ReadLine());
+
+                //Console.WriteLine("Dernières coordonnées :\n");
+                //Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                //dernBordColonne = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                //dernBordLigne = int.Parse(Console.ReadLine());
+                //tools.AfficherGrilleETBateau();
+
+                //Console.WriteLine("Placez le sous-marin (3 cases)\n");
+
+                //Console.WriteLine("Premières coordonnées :\n");
+                //Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                //premBordColonne = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                //premBordLigne = int.Parse(Console.ReadLine());
+
+                //Console.WriteLine("Dernières coordonnées :\n");
+                //Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                //dernBordColonne = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                //dernBordLigne = int.Parse(Console.ReadLine());
+                //tools.AfficherGrilleETBateau();
+
+                //Console.WriteLine("Placez le torpilleur (2 cases)\n");
+
+                //Console.WriteLine("Premières coordonnées :\n");
+                //Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                //premBordColonne = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                //premBordLigne = int.Parse(Console.ReadLine());
+
+                //Console.WriteLine("Dernières coordonnées :\n");
+                //Console.WriteLine("Choisissez la colonne où vous voulez placer votre bateau");
+                //dernBordColonne = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Choisissez la ligne où vous voulez placer votre bateau");
+                //dernBordLigne = int.Parse(Console.ReadLine());
+                //tools.AfficherGrilleETBateau();
+            }
         }
     }
 }
